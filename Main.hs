ca55452b7e4ade9935155c77722a93f76a7b9dd4
Gabriel Pickl <peacemotion@gmail.com>
2015-04-22 18:14:49 +0200

    Beautified error handling.

--- Main.hs
+++ Main.hs
@@ -1,21 +1,35 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 import Control.Monad
 import Control.Exception
 import System.Environment
 import System.Directory
+import System.Console.GetOpt
+import Data.Typeable
+import System.Posix.Types
+import System.IO
 
 import Lib
 import TimeParse
 import Text.Parsec
 
-main = do
-    (path:time:_) <- getArgs
-    case (parse timeParser "Input" time) of 
-        Right t -> do
-            files <- getOld t path
-            print files
-            sequence $ map deleteNode files
-            return ()
-        Left error -> putStr $ show error
+data InputException = InputException deriving (Show, Typeable)
+
+instance Exception InputException
+
+data Options = Options { optFolder :: FilePath
+                       , optTime   :: EpochTime
+                       }
+
+options :: IO Options
+options = do
+    (path:timeStr) <- getArgs
+    case (parse timeParser "stdin" (unwords timeStr)) of
+        Right time -> return Options { optFolder = path
+                                     , optTime = time
+                                     }
+        Left _ -> throw InputException
+
+main = catch (options >>= deleteFiles) (\e -> printError e >> printUsage)
 
 deleteNode :: FilePath -> IO ()
 deleteNode path = catch (do
@@ -26,3 +40,17 @@ deleteNode path = catch (do
     else
         removeFile path
     ) ((putStrLn . show) :: (SomeException -> IO ()))
+    
+deleteFiles opts = do
+    files <- getOld (optTime opts) (optFolder opts)
+    print files
+    sequence $ map deleteNode files
+    return ()
+    
+printError :: InputException -> IO ()
+printError e = hPutStrLn stderr $ show e
+
+printUsage = do
+    programName <- getProgName
+    hPutStrLn stderr "Usage:"
+    hPutStrLn stderr $ programName ++ " <path> <time>"
